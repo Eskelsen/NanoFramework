6a2290458491645ee53a14b2b04dd9f87722d62a
Daniel Eskelsen <eskelsen@yahoo.com>
2025-12-14 13:51:55 -0300

    Introduces base path

--- app/Core/Web.php
+++ app/Core/Web.php
@@ -6,7 +6,7 @@ class Web
 {
     public static function match()
     {
-        $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+        $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), BASE);
         $filepath = $path === '' ? 'streams/home.php' : 'streams/' . $path . '.php';
         $filepath = is_file(APP . $filepath) ? $filepath : 'streams/pages/404.php';
         require APP . $filepath;
